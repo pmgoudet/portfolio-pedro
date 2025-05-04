0c6721ab454c65fcf6252798d449494562779e19
Pedro Mattiazzo Goudet <109755812+pmgoudet@users.noreply.github.com>
2025-05-04 18:38:09 +0200

    j
    j

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import Layout from "../components/Layout";
 import Hero from "../components/Hero";
 import SeparatorLine from "../components/SeparatorLine";
+import SkillCards from "../components/SkillCards";
 
 const App = () => {
   return (
     <Layout>
       <Hero />
       <SeparatorLine />
+      <SkillCards />
     </Layout>
   );
 };
