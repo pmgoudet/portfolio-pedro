79c0aacf7de6d951aa6ebaafda30d54584f8ded6
Pedro Mattiazzo Goudet <109755812+pmgoudet@users.noreply.github.com>
2025-05-06 15:54:15 +0200

    .
    .

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 interface ButtonProps {
   children: React.ReactNode;
   onClick?: () => void;
-  variant?: 'primary' | 'secondary' | 'primary-sm';
+  variant?: 'primary' | 'secondary' | 'primary-sm' | 'secondary-sm';
   disabled?: boolean;
 }
 
@@ -38,10 +38,10 @@ const Button: React.FC<ButtonProps> = ({
 
   if (variant === 'primary-sm') {
     return (
-      <div className="inline-block p-[2px] rounded-md bg-gradient-primary ">
-        <div className="bg-white dark:bg-dark-color rounded-md lg:over:opacity-90 py-2">
+      <div className="inline-block p-[2px] rounded-md bg-gradient-primary shadow-md">
+        <div className="bg-white dark:bg-dark-color rounded-md lg:over:opacity-90 py-0">
           <button
-            className={`w-full h-full bg-white rounded-md text-gradient-primary px-4 ${baseClasses} ${disabledClasses}`}
+            className={`w-full h-full bg-white text-sm rounded-md text-gradient-primary px-2  ${baseClasses} ${disabledClasses}`}
             onClick={onClick}
             disabled={disabled}
           >
@@ -49,6 +49,18 @@ const Button: React.FC<ButtonProps> = ({
           </button>
         </div>
       </div>
+    );
+  }
+
+  if (variant === 'secondary-sm') {
+    return (
+      <button
+        className={`bg-gradient-primary text-white duration-300 px-4 py-1  hover:opacity-90 shadow-md text-sm ${baseClasses} ${disabledClasses}`}
+        onClick={onClick}
+        disabled={disabled}
+      >
+        {children}
+      </button>
     );
   }
 
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -20,30 +20,32 @@ const Project = ({ title, subtitle, description, img, technologies }: ProjectPro
     <section>
       <h2 className='text-2xl font-semibold p-6 pb-2'>Projets</h2>
       <div className="m-6 shadow-lg">
-
         <div className="relative w-full h-40 group overflow-hidden box-border bg-[url(/img/print-minutos.jpg)] bg-cover">
-
-          <div className="absolute inset-0 bg-black bg-opacity-90 flex text-white opacity-0 group-hover:opacity-100 transition-opacity p-2">
+          <div className="absolute inset-0 bg-black bg-opacity-90 text-white opacity-0 group-hover:opacity-100 transition-opacity p-2 hidden lg:flex">
             Seu Texto Aqui ta ligado barato é loco eh nois correndo pelo certo irmao
           </div>
         </div>
 
-        <div className="p-2">
-          <h3 className="text-lg font-semibold pb-2">{title}</h3>
-          <div className='flex gap-2 '>
+        <div className="p-2 flex items-center justify-between">
+          <div className=''>
+            <h3 className="text-lg font-semibold">{title}</h3>
             <p className="text-base">{subtitle}</p>
-            <div className='flex gap-2 pr-2 flex-wrap'>
-              {icons.map((icon) => (
-                <img src={icon} alt={`Logo ${icon}`} className='w-4' />
-              ))}
+          </div>
+          <div className='flex gap-4 mt-2'>
+            <div className='flex gap-2 justify-between flex-col'>
+              <Button variant="primary-sm">GitHub</Button>
+              <Button variant='secondary-sm'>Site</Button>
             </div>
           </div>
-          <div className='flex gap-4 mb-2 mt-4'>
-            <Button variant="primary-sm">Voir sur GitHub</Button>
-            <Button>Voir le site</Button>
+        </div>
+        <div className='w-full h-0'>
+          <p>Seu Texto Aqui ta ligado barato é loco eh nois correndo pelo certo irmao</p>
+          <div className='flex gap-2 pr-2 justify-end items-end '>
+            {icons.map((icon) => (
+              <img src={icon} alt={`Logo ${icon}`} className='w-4' />
+            ))}
           </div>
         </div>
-
       </div>
     </section>
   );
