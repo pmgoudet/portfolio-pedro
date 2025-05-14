60ab3d2a0daca8910fb8439165f4a0344a2ca358
Pedro Mattiazzo Goudet <109755812+pmgoudet@users.noreply.github.com>
2025-05-14 17:11:01 +0200

    m
    m

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -38,9 +38,7 @@ const Header = () => {
     <header className='fixed top-0 left-0 w-full z-50 bg-white dark:bg-dark-color'>
       <div className=' md:relative md:w-auto md:flex md:items-center md:justify-between md:mx-12 md:mb-8 md:mt-0 md:pt-12 lg:mx-32 lg:pt-2 lg:my-6 2xl:mx-60'>
         <div className="flex m-6 mt-0 pt-10 md:p-0 justify-between items-center md:m-0">
-          <a href="#hero">
-            <Logo />
-          </a>
+          <Logo />
           <div ref={burgerRef} className={`tham tham-e-squeeze tham-w-8 z-10 ${activeBurger ? 'tham-active' : ''}`} onClick={toggleBurger}>
             <div className="tham-box md:hidden ">
               <div className="tham-inner bg-black dark:bg-white" />
